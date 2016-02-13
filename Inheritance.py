6ce3529663e8f28ea84f1a95089dbcd45faf70ce
nithya <nithya.semantic@gmail.com>
2016-02-13 17:34:53 +0530

    Class name changes

--- Inheritance.py
+++ Inheritance.py
@@ -1,23 +1,23 @@
-class schoolmember:
+class Schoolmember:
     def __init__(self,name,age):
         self.name=name
         self.age=age
     def display(self):
         print 'name is %s \nand age is %s' %(self.name,self.age)
-class Teacher(schoolmember):
+class Teacher(Schoolmember):
     def __init__(self,salary,name,age):
-        schoolmember.__init__(self,name,age)
+        Schoolmember.__init__(self,name,age)
         self.salary=salary
 
     def display(self):
-        schoolmember.display(self)
+        Schoolmember.display(self)
         print "%s salary is %s" %(self.name,self.salary)
-class Students(schoolmember):
+class Students(Schoolmember):
     def __init__(self,name,age,marks):
-        schoolmember.__init__(self,name,age)
+        Schoolmember.__init__(self,name,age)
         self.marks=marks
     def displyaMakrs(self):
-        schoolmember.display(self)
+        Schoolmember.display(self)
         print "%s marks is %s" %(self.name,self.marks)
 
 
